030e960aa78f256430d21d977f4e23eb72a95cf5
Maria <maria.pandele33@gmail.com>
2020-10-03 19:39:00 +0200

    Add ridge regression model and logistic regression

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -12,39 +12,48 @@ purposes.
 
 
 
-def compute_loss_MSE(e):
-    """Computes the mean squared error from an error vector."""
+def sigmoid(x):
+    """Applies sigmoid function to x."""
+    return 1.0 / (1 + np.exp(-x))
+
+
+def compute_loss_MSE(y, tx, w):
+    """Computes the mean squared error."""
+    e = y - tx.dot(w)
     return 1/2 * np.mean(e**2)
     
 
-def compute_loss_MAE(e):
-    """Computes the mean absolute error from an error vector."""
+def compute_loss_MAE(y, tx, w):
+    """Computes the mean absolute error."""
+    e = y - tx.dot(w)
     return np.mean(np.abs(e))
 
-    
-def compute_loss(y, tx, w, function='MSE'):
-    """Calculate the loss.
-    
-    You can calculate the loss using MSE or MAE.
-    """
-    e = y - tx.dot(w)
-    if function == 'MAE':
-        return compute_loss_MAE(e)
-    else:
-        if function != 'MSE':
-            print("Loss function unknown. Switching to MSE but please double check")
-        return compute_loss_MSE(e)
+
+def compute_loss_rmse(y, tx, w):
+    """Computes the root mean squared error."""
+    return np.sqrt(2 * compute_loss_MSE(y, tx, w))
+
+
+def compute_loss_logistic(y, tx, w):
+    """Computes the loss by negative log likelyhood."""
+    prediction = sigmoid(tx.dot(w))
+    loss = y.T.dot(np.log(prediction)) + (1 - y).T.dot(np.log(1 - prediction))
+    return np.squeeze(-loss)
 
     
 def compute_gradient(y, tx, w):
-    """Computes the gradient of the weight vector in MSE for linear regression.
-    
-    Returns the error as well as the gradient.
-    """
+    """Computes the gradient of the weight vector for linear regression."""
     N = len(y)
     e = y - tx @ w
-    return (-1 / N) * tx.T @ e, e
+    return (-1 / N) * tx.T @ e
 
+
+def compute_gradient_logistic(y, tx, w):
+    """Computes the gradient of the logistic regression loss."""
+    prediction = sigmoid(tx.dot(w))
+    grad = tx.T.dot(prediction - y)
+    return grad
+    
 
 def compute_subgradient(y, tx, w):
     #In case we want to play with non-differentiable functions
@@ -97,8 +106,8 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma, logging=False):
     losses = []
     w = initial_w
     for iter_ in range(max_iters):
-        grad, e = compute_gradient(y, tx, w)
-        loss = compute_loss_MSE(e)
+        grad = compute_gradient(y, tx, w)
+        loss = compute_loss_MSE(y, tx, w)
         w = w - gamma * grad
         ws.append(w)
         losses.append(loss)
@@ -115,8 +124,8 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma, logging=False):
     for iter_ in range(max_iters):
         # This for loop has one iteration.
         for min_batch_y, min_batch_tx in batch_iter(y, tx, batch_size=1, num_batches=1):
-            grad, e = compute_gradient(min_batch_y, min_batch_tx, w)
-            loss = compute_loss_MSE(e)
+            grad = compute_gradient(min_batch_y, min_batch_tx, w)
+            loss = compute_loss_MSE(y, tx, w)
             w = w - gamma * grad
             ws.append(w)
             losses.append(loss)
@@ -125,23 +134,41 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma, logging=False):
     return losses[-1], ws[-1]
 
 
-def least_squares(y,tx):
-    """Calculate the least squares using the normal equations."""
+def least_squares(y, tx):
+    """Calculates the least squares using the normal equations."""
     a = tx.T @ tx
     b = tx.T @ y
     w = np.linalg.solve(a, b)
-    loss = compute_loss(y, tx, w)
+    loss = compute_loss_MSE(y, tx, w)
     return loss, w
 
                   
-def ridge_regression(y,tx,lambda_):
-    #TODO : Ridge regression using normal equations
-    raise NotImplementedError
+def ridge_regression(y, tx, lambda_):
+    """Computes rigde regression using normal equations.
+    
+    The lambda_ parameter is the one used in the minimization formula.
+    """
+    lambda_prime = 2 * lambda_ * len(y)
+    a = tx.T @ tx + (lambda_prime * np.identity(tx.shape[1]))
+    b = tx.T @ y
+    w = np.linalg.solve(a, b)
+    loss = compute_loss_rmse(y, tx, w)
+    return loss, w
 
                   
-def logistic_regression(y,tx,initial_w,max_iters,gamma):
-    #TODO : Logistic regression using gradient descent or SGD
-    raise NotImplementedError
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """Computes logistic regression using gradient descent."""
+    #TODO: Maybe use SGD instead of GD if it is too slow.
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for i in range(max_iters):
+        loss = compute_loss_logistic(y, tx, w)
+        grad = compute_gradient_logistic(y, tx, w)
+        w = w - gamma * grad
+        ws.append(w)
+        losses.append(loss)
+    return losses[-1], ws[-1]
 
                   
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
